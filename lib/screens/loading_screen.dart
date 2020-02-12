5280f861933b5dacdb2cb07cc30d16ed4c26c8d3
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-02-12 19:12:24 +0100

    I am so lost, all I want to do now is start all over and come back later, my task is almost due

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,13 @@
+
 import 'package:flutter/material.dart';
 import 'package:my_weather_app/services/location.dart';
+import 'package:http/http.dart'  as http;
+import 'dart:convert';
+
+
+const apiKey ='100ef8029913a78ceeb3129acb20b497';
+
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -7,30 +15,58 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude;
+  double longitude;
+
 @override
   void initState() {
     super.initState();
     getLocation();
   }
-
+//Updated getLocation(), so that:
   void getLocation() async {
+    //1: creates a new location object
     Location location = Location();
+    //2: Call the getCurrentLocation()
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    //3: print the values stored in longitude & latitude
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+      'http://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+
+          //http://api.openweathermap.org/data/2.5/forecast?lat=37.4219756&lon=-122.0839974&appid=100ef8029913a78ceeb3129acb20b497
+
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main'] ['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+
+    } else{
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        body: Center(
-      //   child: RaisedButton(
-      //     onPressed: () {
-      //       //Get the current location
-      //       getLocation();
-      //     },
-      //     child: Text('Get Location'),
-      //   ),
+      
       ),
     );
   }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,10 +1,12 @@
 import 'package:geolocator/geolocator.dart';
-
+//create a new location class
 class Location {
-
+// create two properties (latitude and longitude)
   double latitude;
   double longitude;
 
+// create a method called getCurrentLocation and move the code snippet of getCurrentPosition in the loading_screen to the getCurrentLocation method.
+// In order to ensure that the getCurrentLocation method completes it's cycle before loading_screen gets currentLocation, we use the 'Future<void>, instead of just "void"
   Future<void> getCurrentLocation() async {
     try{ 
     Position position = await Geolocator()
