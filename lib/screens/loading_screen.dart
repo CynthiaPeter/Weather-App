61d0f60f784bb41cc7a6117b2571991d305e0084
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-02-12 09:42:26 +0100

    fixed bug and modified location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart'; 
-
+import 'package:my_weather_app/services/location.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -8,24 +7,30 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+@override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   void getLocation() async {
-    Position position = await Geolocator()
-    .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-        print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
+       body: Center(
+      //   child: RaisedButton(
+      //     onPressed: () {
+      //       //Get the current location
+      //       getLocation();
+      //     },
+      //     child: Text('Get Location'),
+      //   ),
       ),
     );
   }
